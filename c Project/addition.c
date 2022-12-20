d75d4c76a7782543f99b1c2c5aeaa86f3e4a9bee
AniketSP4912 <121081785+AniketSP4912@users.noreply.github.com>
2022-12-20 12:18:19 +0530

    update add.c

--- c Project/addition.c
+++ c Project/addition.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 main()
 {
-	int a,b,c;
+	int a,b,c,m;
 	printf("\n enter the value of a");
 	scanf("%d",&a);
 		printf("\n enter the value of b");
 	scanf("%d",&b);
 	c=a+b;
 		printf("\n addition of a and b= ",c);
+	m=a*b;
+	printf("|n multiplication of a and b = ",m);
 	
 }
