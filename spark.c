7a41917ae4f3cda30612a451d45c66e6599f5988
AniketSP4912 <121081785+AniketSP4912@users.noreply.github.com>
2022-12-20 12:05:47 +0530

    Update spark.c

--- spark.c
+++ spark.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
+void add();
 main()
 {
 printf("\n wellcome");
+  printf("\n Aniket"):
+  add();
+}
+void add()
+{
+	int a,b,c;
+	printf("\n enter the value of a");
+	scanf("%d",&a);
+		printf("\n enter the value of b");
+	scanf("%d",&b);
+	c=a+b;
+		printf("\n addition of a and b= ",c);
 }
